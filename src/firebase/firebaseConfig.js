d8555b70ea3dfc23ad12bc9bc7fbb794d3a9bf9b
tanishk marothiya <tanishkmarothiya1212@gmail.com>
2023-04-08 21:44:32 +0530

    firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,12 +1,25 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDdWhflG9YHRguJBwCbu9g1b09XJYynQdY",
-    authDomain: "hostel-management-101.firebaseapp.com",
-    databaseURL: "https://hostel-management-101.firebaseio.com",
-    projectId: "hostel-management-101",
-    storageBucket: "hostel-management-101.appspot.com",
-    messagingSenderId: "815637251373",
-    appId: "1:815637251373:web:820cb8a2fde77d50b07fa4",
-    measurementId: "G-G9TDH558S0"
-};
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDdWhflG9YHRguJBwCbu9g1b09XJYynQdY",
+//     authDomain: "hostel-management-101.firebaseapp.com",
+//     databaseURL: "https://hostel-management-101.firebaseio.com",
+//     projectId: "hostel-management-101",
+//     storageBucket: "hostel-management-101.appspot.com",
+//     messagingSenderId: "815637251373",
+//     appId: "1:815637251373:web:820cb8a2fde77d50b07fa4",
+//     measurementId: "G-G9TDH558S0"
+// };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBSo35pdFTnct-Gw_PfctkNQ9BtTXEHNhc",
+    authDomain: "hostel-management-fe873.firebaseapp.com",
+    databaseURL: "https://hostel-management-fe873-default-rtdb.firebaseio.com",
+    projectId: "hostel-management-fe873",
+    storageBucket: "hostel-management-fe873.appspot.com",
+    messagingSenderId: "669211232481",
+    appId: "1:669211232481:web:f49d95198b194ac2ee0fa0",
+    measurementId: "G-N65W08MDW1"
+  };
+
+
 
 export default firebaseConfig;
